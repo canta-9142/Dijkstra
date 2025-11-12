9c8aad904fb73e23b2410b460ae65b0ae5253919
今井 寛大 <161188313+canta-9142@users.noreply.github.com>
2025-11-13 03:58:15 +0900

    Refactor RandomHeap to extend Heap and update methods

--- Dijkstra/RandomHeap.java
+++ Dijkstra/RandomHeap.java
@@ -1,40 +1,43 @@
-// FiboHeapクラスのpop()が距離優先ではなくランダムに呼び出されるようにしたもの
+public class RandomHeap extends Heap
+{
+    private Node oldest;
 
-import static processing.core.PApplet.*;
-
-public class RandomHeap extends FiboHeap {
-  public RandomHeap() {
-    super();
-  }
-
-  @Override
-  protected void setNewHead() {
-    if (head == null) return;
-    // できるだけあとに追加されたものが優先されるようにheadを決める
-    // 全体の高さの1/4までは入り込んで取り出せるスタックみたいなイメージ
-    double r = Math.random(); // rは0~1の少数
-    int index = floor((float)Math.pow(r - 0.5, 2) * this.getN()); // n(ルートリストの数 = headを回転できる最大回数)×0~1/4までの整数を指定
-    for (int i = 0; i < index; i++) {
-      head = head.left; // 追加されたタイミングが新しい順に見ていく
+    public RandomHeap()
+    {
+        super();
+        this.oldest = null;
     }
-  }
-  @Override
-  public void pop() {
-    if (head == null) return;
-    if (head == head.right) {
-      head = null;
-    } else {
-      Node r = head.right;
-      head.remove();
-      head = r;
+
+    @Override
+    public void push(Node node)
+    {
+        super.push(node);
+        if (oldest == null) oldest = node;
+        return;
     }
-    n--;
-    return;
-  }
-  public int popRandom() {
-    this.setNewHead();
-    int top = this.head.index;
-    this.pop();
-    return top;
-  }
+
+    @Override
+    public Node pop()
+    {
+        if (head == null) return null;
+        setNewHead();
+        Node min = head;
+        head.peel(head);
+        head.remove();
+        n--;
+        head = min == oldest ? null : oldest;
+        return min;
+    }
+
+    @Override
+    protected void setNewHead()
+    {
+        if (head == null) return;
+
+        double r = Math.random();
+        int index = (int)Math.floor(Math.pow(r - 0.5, 2) * this.n);
+        Node cur = head;
+        for (int i = 0; i <= index; i++) cur = cur.left;
+        head = cur;
+    }
 }
