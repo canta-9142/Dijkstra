0a1707ca970cf8a2a5d5fac723466ccc25662cee
今井 寛大 <161188313+canta-9142@users.noreply.github.com>
2025-11-13 04:03:13 +0900

    Modify RandomHeap to change index calculation in setNewHead
    Updated the index calculation in setNewHead method to adjust the random selection logic.

--- Dijkstra/RandomHeap.java
+++ Dijkstra/RandomHeap.java
@@ -13,7 +13,7 @@ public class RandomHeap extends FiboHeap {
     // できるだけあとに追加されたものが優先されるようにheadを決める
     // 全体の高さの1/4までは入り込んで取り出せるスタックみたいなイメージ
     double r = Math.random(); // rは0~1の少数
-    int index = floor((float)Math.pow(r - 0.5, 2) * this.getN()); // n(ルートリストの数 = headを回転できる最大回数)×0~1/4までの整数を指定
+    int index = floor((float)Math.pow(r - 0.5, 2) / 1.5f * this.getN()); // n(ルートリストの数 = headを回転できる最大回数)の0~1/4の整数を指定
     for (int i = 0; i < index; i++) {
       head = head.left; // 追加されたタイミングが新しい順に見ていく
     }
